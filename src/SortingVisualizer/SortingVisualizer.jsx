1fe3414bd4cffda0fd0a16ae3dfd84b5af234830
meyraud <65074947+meyraud@users.noreply.github.com>
2020-05-12 01:31:12 +0200

    Update SortingVisualizer.jsx

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -6,7 +6,7 @@ import {getBubbleSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js
 import './SortingVisualizer.css';
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 0.2;
+const ANIMATION_SPEED_MS = 0.25;
 
 // Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 92;
